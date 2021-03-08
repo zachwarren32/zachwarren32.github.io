1abdd3071960cd9fdca702dfaa4568da63f6c855
zachwarren32 <zachwarren32@gmail.com>
2021-03-08 16:49:21 -0700

    commit

--- zachwarren32.github.io/currentdate.js
+++
@@ -1,7 +0,0 @@
-var string = document.lastModified;
-
-document.getElementById('currentdate').textContent = string;
-
-var d = new Date();
-var n = d.getFullYear();
-document.getElementById('year').textContent = n;
--- zachwarren32.github.io/lesson-8/stormcenter.js/stormcenter.js
+++
@@ -1,3 +0,0 @@
-function adjustRating(rating) {
-    document.getElementById("severityvalue").innerHTML = rating;
-}
